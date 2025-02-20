97dffa91d0d4325f7b3057d9c8d87ce69ac048fd
Daniel Fatokun <danielfatokun@gmail.com>
2025-02-20 13:43:15 -0700

    Updated layout.tsx and added login.tsx

---
+++ day2soap/frontend/day2soap/src/components/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <html>
+        <body>
+            <div className=''>
+                login
+            </div>
+        </body>
+    </html>
+  )
+}
