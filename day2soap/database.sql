93f604d237838a112e96d6ce15ff3b05a16aa1d9
marcus lieu <marcus.lieu4@gmail.com>
2025-02-21 05:31:57 -0700

    changed user table in sql file and also added user logins

--- day2soap/database.sql
+++ day2soap/database.sql
@@ -4,9 +4,8 @@ USE MusicRecommendationDB;
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+	username VARCHAR(50) NOT NULL,
+	user_password VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE recommended_songs (
