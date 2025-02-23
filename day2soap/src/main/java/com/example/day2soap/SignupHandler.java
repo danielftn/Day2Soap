b590b4e86e2ad9ee1c5f41c9902652dc9898fa46
marcus lieu <marcus.lieu4@gmail.com>
2025-02-23 01:42:49 -0700

    removed un needed package

--- day2soap/src/main/java/com/example/day2soap/SignupHandler.java
+++ day2soap/src/main/java/com/example/day2soap/SignupHandler.java
@@ -3,7 +3,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import java.sql.Connection;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
