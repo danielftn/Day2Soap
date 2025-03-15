51c92cdc8265ba4049f63a8ae0ca68b301976e82
marcus lieu <marcus.lieu4@gmail.com>
2025-03-14 18:51:59 -0600

    changed primary key in users to be username

--- day2soap/database.sql
+++ day2soap/database.sql
@@ -3,18 +3,17 @@ CREATE DATABASE day2soapdb;
 USE day2soapdb;
 
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(50) NOT NULL,
+	username VARCHAR(50) PRIMARY KEY,
 	user_password VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE recommended_movies (
     recommendation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
+    user VARCHAR(50),
     movie_title VARCHAR(255) NOT NULL,
     release_year INT,
     description TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (user) REFERENCES users(username) ON DELETE CASCADE
 );
 
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'ucalgary';
