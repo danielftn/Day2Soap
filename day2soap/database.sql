d663b037b6e108946fb8f8265dc57810f2f06837
Aiden Lambert <aiden.lambert@ucalgary.ca>
2025-03-16 16:57:07 -0600

    Got rid of test statements in SQL file

--- day2soap/database.sql
+++ day2soap/database.sql
@@ -20,17 +20,5 @@ CREATE TABLE recommended_movies (
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'ucalgary';
 GRANT ALL PRIVILEGES ON day2soapdb.* TO 'user'@'localhost';
 
-INSERT INTO recommended_movies (user, movie_title, release_year, description, watched) VALUES 
-("test", "the matrix", 2025, "test desc", true);
-
-INSERT INTO recommended_movies (user, movie_title, release_year, description) VALUES 
-("test", "cars", 2025, "test desc");
-
-INSERT INTO recommended_movies (user, movie_title, release_year, description) VALUES 
-("test", "GOAT MOVIE", 1900, "GOAT desc");
-
-INSERT INTO recommended_movies (user, movie_title, release_year, description) VALUES 
-("daniel", "spiderverse", 2019, "test desc");
-
 Select * from users;
 Select * from recommended_movies;
