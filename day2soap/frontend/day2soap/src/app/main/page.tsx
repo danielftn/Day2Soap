a67bd0976a5c129ab6f8bf3b3ac2c6f2d527e164
Daniel Fatokun <danielfatokun@gmail.com>
2025-03-20 21:39:51 -0600

    Removed unused imports

--- day2soap/frontend/day2soap/src/app/main/page.tsx
+++ day2soap/frontend/day2soap/src/app/main/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 import React from 'react'
 import Main from '../../components/main'
-import { useUser } from '@/context/UserContext'
 
 export default function page() {
-  const { user } = useUser();
-  console.log(user);
   return (
     <main>
         <Main/>
